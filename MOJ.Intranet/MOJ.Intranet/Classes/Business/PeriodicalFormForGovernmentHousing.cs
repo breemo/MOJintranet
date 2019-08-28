72ba0805ddc75b1e4da3040c004ae4dcbda24eb8
Naser Asad <Naser_asad@hotmail.com>
2019-08-28 22:50:20 +0400

    PeriodicalFormForGovernmentHousing wp fieald

---
+++ MOJ.Intranet/MOJ.Intranet/Classes/Business/PeriodicalFormForGovernmentHousing.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MOJ.DataManager;
+using MOJ.Entities;
+
+namespace MOJ.Business 
+{
+    public class PeriodicalFormForGovernmentHousing
+    {
+        public bool SaveUpdate(PeriodicalFormForGovernmentHousingEntity obj)
+        {
+            return new PeriodicalFormForGovernmentHousingDataManager().AddOrUpdate(obj);
+        }
+        
+            public bool SaveUpdateChildren(List<SonsEntity> obj)
+        {
+            return new SonsDataManager().AddOrUpdateHostingChildren(obj);
+        }
+        
+                public bool SaveUpdateHusbandORWife(List<HusbandORWifeEntity> obj)
+        {
+            return new HusbandORWifeDataManager().AddOrUpdateHusbandORWife(obj);
+        }
+
+        public PeriodicalFormForGovernmentHousingEntity GetAffirmationSocialSituation(int id)
+        {
+            return new PeriodicalFormForGovernmentHousingDataManager().GetByID(id);
+        }
+        
+
+            public List<HusbandORWifeEntity> GetHusbandORWife(string title)
+        {
+            return new HusbandORWifeDataManager().GetHusbandORWife(title);
+        }
+        
+         public List<SonsEntity> Getsons(string title)
+        {
+            return new SonsDataManager().Getsons(title);
+        }
+
+        
+    }
+}
